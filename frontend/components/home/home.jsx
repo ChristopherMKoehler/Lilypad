81498206b916a5a43214f7a80d96265e4cb905c1
Kevin Dam <kevinddam@gmail.com>
2017-04-21 13:59:53 -0400

    Delete reference to greeting component

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from '../navbar/navbar';
 import HomeHeaderContainer from '../header/home_header_container';
-import GreetingContainer from '../greetings/greeting_container';
 
 const home = (props) => {
   return (
