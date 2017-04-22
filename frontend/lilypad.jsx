72d6457479f6a1484d0cae9ae096b1cb31b4afd4
Chris Koehler <christophermnkoehler@gmail.com>
2017-04-22 14:52:35 -0400

    Add fix to resizing of the side bar

--- frontend/lilypad.jsx
+++ frontend/lilypad.jsx
@@ -13,7 +13,6 @@ document.addEventListener("DOMContentLoaded", () => {
     store = configureStore();
   }
   window.store = store;
-  window.ListApiUtil = ListApiUtil;
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={ store }/>, root);
 });
